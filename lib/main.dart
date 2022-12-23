5afeeca4ad5764f64fd8861fc7dfcb649a175e1c
reisalazar <rrsb45@gmail.com>
2022-12-22 21:11:59 -0300

    Column to rganize Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(PerguntaApp());
+void main() {
+  runApp(const QuestionsApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class QuestionsApp extends StatelessWidget {
+  const QuestionsApp({super.key});
+
+  answer() {
+    print('Question answered!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      "What's your favorite color?",
+      "Whats's your favorite animal?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas'),
+          title: const Text('Questions'),
         ),
-        body: Text('OláFlutter!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: answer(),
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: answer(),
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: answer(),
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
