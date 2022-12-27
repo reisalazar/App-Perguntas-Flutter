3fc4dad9e1f8ef3cf6b2d8132fc74799f8a12ebb
reisalazar <rrsb45@gmail.com>
2022-12-27 20:47:10 -0300

    Callback

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,14 +2,22 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final String texto;
+  final void Function() onSelected;
 
-  Answer(this.texto);
+  const Answer(this.texto, this.onSelected);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: Text(texto),
-      onPressed: null,
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+        onPressed: onSelected,
+        child: Text(texto),
+      ),
     );
   }
 }
