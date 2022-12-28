e8eef9062bd69226ece4288c646358e8dc87eaec
reisalazar <rrsb45@gmail.com>
2022-12-28 20:12:58 -0300

    null and out of bounds fix

--- lib/main.dart
+++ lib/main.dart
@@ -6,43 +6,52 @@ main() => runApp(const QuestionsApp());
 
 class _QuestionsAppState extends State<QuestionsApp> {
   var _questionSelected = 0;
+  final _questions = const [
+    {
+      "text": "What's your favorite color?",
+      "answer": ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      "text": "What's your favorite animal?",
+      "answer": ['Bunny', 'Snake', 'Cat', 'Dog']
+    },
+    {
+      "text": "What's you favorite teacher?",
+      "answer": ['Maria', 'John', 'Leo', 'Peter'],
+    }
+  ];
 
   void _answer() {
-    setState(() {
-      _questionSelected++;
-    });
+    if (hasQuestionSelected) {
+      setState(() {
+        _questionSelected++;
+      });
+    }
+  }
+
+  bool get hasQuestionSelected {
+    return _questionSelected < _questions.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> questions = [
-      {
-        "text": "What's your favorite color?",
-        "answer": ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        "text": "What's your favorite animal?",
-        "answer": ['Bunny', 'Snake', 'Cat', 'Dog']
-      },
-      {
-        "text": "What's you favorite teacher?",
-        "answer": ['Maria', 'John', 'Leo', 'Peter'],
-      }
-    ];
-
-    List<String> answers = questions[_questionSelected].cast()['answer'];
+    List<String> answers = hasQuestionSelected
+        ? _questions[_questionSelected].cast()['answer']
+        : [];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Questions'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionSelected]['text'] as String),
-            ...answers.map((t) => Answer(t, _answer)).toList(),
-          ],
-        ),
+        body: hasQuestionSelected
+            ? Column(
+                children: [
+                  Question(_questions[_questionSelected]['text'] as String),
+                  ...answers.map((t) => Answer(t, _answer)).toList(),
+                ],
+              )
+            : null,
       ),
     );
   }
